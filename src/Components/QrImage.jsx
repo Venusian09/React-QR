20f2db68e7373a51e05a24577c12b397fb2235f1
Venusian09 <kdebczak@edu.cdv.pl>
2023-02-28 20:47:32 +0100

    styles and small script change

--- src/Components/QrImage.jsx
+++ src/Components/QrImage.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 export default function QrImage({ text }) {
   const url = `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${text}`;
-  const downloadSVGUrl = `https://api.qrserver.com/v1/create-qr-code/?size=1000x1000&data=${text}&format=svg`;
   return (
     <div className="qr__fetch">
       <img src={url} alt={text} />
-      <a className="qr__download" href={downloadSVGUrl} target="_blank">
+      <a
+        className="qr__download"
+        href={`${url}&format=svg`}
+        target="_blank"
+        rel="noreferrer"
+      >
         Pobierz SVG
       </a>
     </div>
